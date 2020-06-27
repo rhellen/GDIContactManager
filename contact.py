41d5ad702fdb92b631c11271966f0d4a0f2d1453
rrame <rramesh1217@gmail.com>
2020-06-27 13:51:16 -0700

    merging Melanie's changes

--- contact.py
+++ contact.py
@@ -1,14 +1,18 @@
+import random
+import re
 import sys
-import csv
+
+from random import choice
 
 contacts = []
+uids = []
 #contacts = [['Clark','Melanie','Melanie Clark','asldfj@lskdjf','23948-234'],['mel','Clark','Mel Clark','lskdfj@slkdjf','234']]
 
 def menu():
 
     print("\nCommand Menu\n")
     print("list - Display all contacts")
-    print("view - View a contact\n")
+    print("view - View a contact")
     print("add - Add a contact")
     print("del - Delete a contact")
     print("exit - Exit program\n")
@@ -20,14 +24,14 @@ def menu():
         if choice == "list":
             all_contacts()
         elif choice == "view":
-            print("Command entered:  View")
+            print("Command:  View")
             search = input("Search Name (first and last): ")
             view(search)
         elif choice == "add":
             add()
         elif choice == "del":
             print("Command:  Delete")
-            search = input("Search Name: ")
+            search = input("Search Name (first and last): ")
             matches = view(search)
             if len(matches) <= 0:
                 print("\nNo Matches Found.")
@@ -39,7 +43,7 @@ def menu():
             print("\nYou must only select either list, view, del, or exit.")
             print("Please try again")
         menu()
-    
+
     print("Exiting the Contact Manager")
     #Contacts are not to be saved
     contacts=[]
@@ -48,44 +52,43 @@ def menu():
 def all_contacts():
 #lists all the contacts
     print("\nCommand: List")
-    global contacts 
+    global contacts
     if active_list():
-        for i, (lname,fname,fullname,email,number) in enumerate(contacts):
-            print("{}. {}  Email:{}  Phone:({})".format(i+1,fullname,email,number))
-            print()
-        
+        for i in contacts:
+            print(i)
+
 def view(search):
 #list one contact
     search_list = []
-    if active_list():        
+    if active_list():
         cnt = 0
-        for item in contacts:
-            if search.upper() in str(item).upper():
+        for entry in contacts:
+            if search.upper() in str(entry).upper():
                 #Display items that are possible matches and store
-                print("{}. {}  Email:{}  Phone:({})".format(cnt+1,item[2],item[3],item[4]))
-                search_list.append(item)                
+                print(entry)
+                search_list.append(entry)
                 cnt += 1
     return search_list
 
 def getname():
 
-    fullname = input("Name (first and last): ")
-    lname = None
-    fname = None
+    verified_name = False
 
-    if not is_fullname(fullname):
-        getname()
+    #check if the user has entered their fullname before proceding
+    while not verified_name:
+        fullname = input("Name (first and last): ")
+        verified_name = is_fullname(fullname)
+
+    #create the variables to hold first name and last name
+    fname, lname = fullname.split(" ")
+
+    #check First Name and Last Name are correct.
+    chk = input(f"Is the full name *{fname} {lname}* correct? (Y/N) ")
+    accepted_answers = ["Y", "y", "YES", "yes", "yup", "Yes"]
     
-    #Check First Name and Last Name are correct.
-    chk = input(f"Is the full name {fname} {lname} correct? (Y/N)")
-   
-    while chk != "Y":
-        lname = input("Enter the last name: ")
-        lname = lname.capitalize()
-        fname = input("Enter the first name: ")
-        fname = fname.capitalize()
-        fullname = fname + " " + lname
-        chk = input(f"Is the full name {fname} {lname} correct? (Y/N)")
+    if chk not in accepted_answers:
+        #re-run the function
+        getname()
 
     return fullname, lname, fname
 
@@ -96,38 +99,70 @@ def is_fullname(name):
     except ValueError:
         print("\nLast and first name required.")
         return False
-        
+
 def add():
 #(1)Identifies for duplicate names.
 #(2)Adds one new contact.
-    
+
     global contacts
+    random_uid = None
+
+    if len(uids) >0:
+        # create a new random UID for this addition, and then append the UID list
+        random_uid = choice([i for i in range(0,100) if i not in uids])
+    else:
+        random_uid = random.randint(0, 100)
+
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 
     print("\nCommand:  Add")
-    fullname, lname, fname = getname()    
+    fullname, lname, fname = getname()
+    
     email = input("Email: ")
-    num = input("Phone: \n")
+    email_validated = re.search(regex, email)
 
-   #Check if Contact Exists   
+    #check if the e-mail is the correct format
+    while not email_validated:
+        email = input("Please enter a valid e-mail: ")
+        email_validated = re.search(regex, email)
+
+    while True:
+        phone_num_entered = input("Please Enter in a Phone Number (+1,xxx-xxx-xxxx): ")
+        valid,formatted_num = get_phone(phone_num_entered)
+        if valid == True:
+            break
+        else:
+            chk = input("\nWould you like to try again? (Y/N)")
+            if chk[:1].upper() == "Y":                
+                continue
+            elif chk[:1].upper() == "N":
+                print("\nPhone Number couldn't be validated.")
+                break    
+    print (f"The phone number added is {formatted_num}")
+
+   #Check if Contact Exists
     res1 = any(fullname in sublist for sublist in contacts)
-    if res1 == True:
 
-        print("\nWarning:  This Contact May Exists")
+    if res1 == True:
         
+        print(f"Warning: A similar contact was found.  This may be a duplicate. {res1}")
+
         while True:
             chk = input("\nWould you like to continue? (Y/N)")
             if chk[:1].upper() == "Y":
-                contacts.append([lname,fname,fullname,email,num])
-                print("\n\n" + fullname + " was added.")
+                contacts.append([random_uid,lname,fname,fullname,email,formatted_num])
+                uids.append(random_uid)
+                print(f"\n\n {fullname} was added")
                 break
                 #print(contacts)
             elif chk[:1].upper() == "N":
                 print("\nContact Not Added")
-                break      
+                break
     else:
-        contacts.append([lname,fname,fullname,email,num])
-        print(fullname + " was added.")
-    
+        contacts.append([random_uid, lname,fname,fullname,email,formatted_num])
+        uids.append(random_uid)
+        print(f"{fullname} was added.")
+
 def active_list():
     if len(contacts) > 0:
         return True
@@ -141,34 +176,130 @@ def is_number(x):
         return True
     except ValueError:
         return False
-    
+
 def delete(matches):
 #(1)Displays lists of contacts
 #(2)User selects one contact to delete
 
-    rst = input("\nSelect a contact to be deleted from 1 to " + str(len(matches))+": ")
+    record_to_delete = input("\nSelect a contact to be deleted by entering their User ID: ")
 
     try:
-        if is_number(rst) == True:
-            rst = int(rst)
-            print(rst)
-            if rst > 0 and rst <= len(matches):
-                contacts.remove(matches[rst-1])
-                print("\nDelete successful.")
-            else:
-                print("\nInvalid Selection.")
+        if is_number(record_to_delete):
+            record_to_delete = int(record_to_delete)
+
+            #loop through all the entries and look at the first element
+            records_deleted = 0
+            
+            for entry in contacts:
+                if int(entry[0]) == record_to_delete:
+                    contacts.remove(entry)
+                    print("\nDelete successful.")
+                    records_deleted += 1
+            if records_deleted == 0:
+                print("The record you've selected does not exist.  Please try again.")
                 menu()
-        else:
-            print("\nInvalid selection. Try again.")
-            menu()        
     except ValueError:
         print("\nInvalid selection.")
-        menu()        
+        menu()
+
+def get_phone(phone):
     
+    try:
+        line = phone
+
+        #line = "+1,(716)545-2718"
+        #line = "+1,716 545-2718"
+        #line = "+44 20 7987 0656"
+        #line = "+1,716-454-1234"
+        #line = "1,716 545 1212"
+        #line = "(716)545 2718"
+        #line = "7165452718"
+        #line = "5454545"
+        #line = "(716)lll545 2718"
+        #line = "+1,7165452718"
+        #line = "+17165452718"  Can't handle this case
+
+        #Allowed characters striping letters
+        newline = ""
+        allowed = ['0','1','2','3','4','5','6','7','8','9','+','(',')'," ",',','-','.']        
+        for char in line:
+            if char in allowed:
+                newline += char
+
+        #Remove all extra characters in list and replace with space
+        line = newline
+        line = replaceMultiple(line, ['(' ,')' ,'.' ,'-' ,','] , " ")
+        
+        #Split the results
+        line = line.lstrip()
+        line = line.split(" ")
+        rst = len(line)
+        
+        #International
+        if "+" in line[0]:
+
+            #"+ 1,716-454-1234"
+            if rst == 5:
+                return True,"{}{},({}){}-{}".format(line[0],line[1],line[2],line[3],line[4])
+            #"+1,716-454-1234"
+            elif rst == 4:
+                return True,"{},({}){}-{}".format(line[0],line[1],line[2],line[3])
+            elif rst == 2 and len(line[1])==10:
+                numpart = str(line[1])
+                return True, "{}({}){}-{}".format(line[0],numpart[0:3],numpart[3:6],numpart[6:10])
+            else:
+                print(f"Possible Error, International number (+1,xxx-xxx-xxxx): {phone}")
+                return False, phone
+
+        #Possible International
+        #"1,716-545-2718"
+        elif rst == 4:
+            return True, "+{},({}){}-{}".format(line[0],line[1],line[2],line[3])
+
+        #line = "(716)545 2718"
+        elif rst == 3:
+            if len(line[0])==3 and len(line[1])==3 and len(line[2])==4:
+                return True, "({}){}-{}".format(line[0],line[1],line[2])
+        #line="545 2121"
+        elif rst == 2:
+            if len(line[0])==3 and len(line[1])==4:
+                return True, "{}-{}".format(line[0],line[1])
+        elif rst == 1:
+            numpart = str(line[0])
+            if len(line[0])==10:                
+                return True, "({}){}-{}".format(numpart[0:3],numpart[3:6],numpart[6:10])
+            elif len(line[0])==7:                
+                print ("Invalid format: {}-{}".format(numpart[0:3],numpart[3:6],numpart[6:9]))
+                return False, phone                
+            else:
+                print(f"Possible Error, number (+1,xxx-xxx-xxxx): {phone}")
+                return False, phone
+        else:
+            print(f"Possible Error, number (+1,xxx-xxx-xxxx): {phone}")            
+            return False, phone
+                 
+    except ValueError:
+        print(f"Error: phone number (+1,xxx-xxx-xxxx): {phone}")
+        return False, phone
+        
+        
+        
+def replaceMultiple(mainString, toBeReplaces, newString):
+    # Iterate over the strings to be replaced
+    print(newString)
+    for elem in toBeReplaces :
+        # Check if string is in the main string
+        if elem in mainString :
+            # Replace the string
+            mainString = mainString.replace(elem, newString)
+    #if mainString[0] in "-" :
+    #            mainString = mainString.replace("-", "" , 1)
+                
+    return  mainString
 
 def main():
     print("\nWelcome to Contact Manager\n")
     menu()
-    
+
 if __name__ == "__main__":
     main()
